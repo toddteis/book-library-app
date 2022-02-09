fcb93bc4be159df5381202fd8bd5c7da556df884
toddt <toddteis@gmail.com>
2022-02-09 14:16:09 +1000

    Created basic function shells for addBookToLibrary() & getAllBooksFromLibrary(), also function constructor Book().

---
+++ main.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book() {
+    // constructor: Author, Title, Category,
+    // N# of pages, Read Status, Review
+}
+
+function addBookToLibrary() {
+    // do stuff here
+}
+
+function getAllBooksFromLibrary() {
+    //returns all books from the libaray
+}
