af44a26c6742e267656a4365ef6c866a7fc70724
toddt <toddteis@gmail.com>
2022-03-11 10:41:21 +1000

    Successfully added book manually from js to UI.

--- main.js
+++ main.js
@@ -27,12 +27,36 @@ function getAllBooksFromLibrary() {
     return result;
 }
 
+// Test: add one book manually to the page.
+let tableRow = document.createElement('tr');
+let tbTitle = document.createElement('td');
+tbTitle.textContent = 'The Hobbit';
+tableRow.append(tbTitle);
+let tbAuthor = document.createElement('td');
+tbAuthor.textContent = 'Tolkien';
+tableRow.append(tbAuthor);
+let tbCategory = document.createElement('td');
+tbCategory.textContent = 'Fantasy';
+tableRow.append(tbCategory);
+let tbPages = document.createElement('td');
+tbPages.textContent = 1000;
+tableRow.append(tbPages);
+let tbReadStatus = document.createElement('td');
+tbReadStatus.textContent = 'no';
+tableRow.append(tbReadStatus);
+let tbReview = document.createElement('td');
+tbReview.textContent = 'no';
+tableRow.append(tbReview);
+console.log(tableRow);
+display.append(tableRow);
+
+
 function createHtmlPage() {
     let arr = getAllBooksFromLibrary();
     for (let i = 0; i < arr.length; i++) {
         let arr2 = Object.values(arr[i]);
         for (let j = 0; j < arr2.length; j++) {
-            console.log(arr2[j]);
+            // console.log(arr2[j]);
         }
     }
 }
